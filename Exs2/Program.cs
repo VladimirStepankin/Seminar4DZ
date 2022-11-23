752085c3121d1728de0b7e3b0c5b6e5ff61c8bb3
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-11-23 20:08:04 +0300

    Добавил вторую задачу

---
+++ Exs2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе*/
+
+int number = ReadInt("Введите число: ");
+
+int sum = 0;
+
+while (number > 0) 
+{
+    int digit = number % 10;
+    sum += digit;
+    number = number / 10;
+}
+
+Console.Write($"Сумма цифр равна {sum}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
